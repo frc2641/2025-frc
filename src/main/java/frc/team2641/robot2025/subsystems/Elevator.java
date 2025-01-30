8ac5fe1811e1bde65d97f521064bbe4510a33371
Aidan Liddy <aidan@aidanliddy.com>
2025-01-30 07:07:32 -0500

    Cleanup

--- src/main/java/frc/team2641/robot2025/subsystems/Elevator.java
+++ src/main/java/frc/team2641/robot2025/subsystems/Elevator.java
@@ -1,23 +1,19 @@
 package frc.team2641.robot2025.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.team2641.robot2025.Robot;
 
 public class Elevator extends SubsystemBase {
-    private static Elevator instance;
+  private static Elevator instance;
 
-    public static Elevator getInstance() {
-      if (instance == null)
-        instance = new Elevator();
-      return instance;
-    }   
+  public static Elevator getInstance() {
+    if (instance == null)
+      instance = new Elevator();
+    return instance;
+  }
 
-    private Elevator()
-    {
+  private Elevator() {
+  }
 
-    }
-
-    
   @Override
   public void periodic() {
   }
